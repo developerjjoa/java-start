13ca914466989d15cfd55c6bd40b5ffeb4ad814e
developerjjoa <developerjjoa@gmail.com>
2025-06-28 23:27:52 +0900

    변수 타입 1

---
+++ src/variable/Var7.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b = 10.5; //실수
+        boolean c = true; //불리언(boolean) true, false 2가지만 입력 가능
+        char d = 'A'; //문자 하나
+        String e = "Hello Java"; //문자열, 문자열을 다루기 위한 특별한 타입
+
+        //int z = "백원"; //정수 타입에 문자열(X)
+        //int z = "100"; //정수 타입에 문자열(X) 숫자 100이 아니라 문자열 "100"
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
