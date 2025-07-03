8d20628b8d57bf61aaced8249c2b21502bf581c5
developerjjoa <developerjjoa@gmail.com>
2025-07-04 00:59:10 +0900

    배열이 필요한 이유

---
+++ src/array/Array1.java
@@ -0,0 +1,23 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int student1 = 90;
+        int student2 = 80;
+        int student3 = 70;
+        int student4 = 60;
+        int student5 = 50;
+
+        /* 변수명은 더하거나 뺄 수 있는게 아니다. 그대로 바로 써야함.
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("학생" + i + " 점수: " + student + i);
+        }
+        */
+
+        System.out.println("학생1 점수: " + student1);
+        System.out.println("학생2 점수: " + student2);
+        System.out.println("학생3 점수: " + student3);
+        System.out.println("학생4 점수: " + student4);
+        System.out.println("학생5 점수: " + student5);
+    }
+}
